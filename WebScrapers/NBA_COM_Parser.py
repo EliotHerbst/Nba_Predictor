7e6efd29d5bc69ffbc93ec92e5ee460877713844
EliotHerbst <39841180+EliotHerbst@users.noreply.github.com>
2020-01-28 00:22:21 -0500

    Update NBA_COM_Parser.py

--- WebScrapers/NBA_COM_Parser.py
+++ WebScrapers/NBA_COM_Parser.py
@@ -4,7 +4,14 @@ Created on Sun Nov 17 17:50:08 2019
 
 @author: Eliot Herbst
 """
-
+# Grabbing text in between any html tags and turn it into a BeautifulSoup object
+def getText(list):
+    texts = []
+    for string in list:
+        soupy = BeautifulSoup((str("<html>") + str(string) + str("</html>")), 'html.parser')
+        text = soupy.get_text()
+        texts.append(text)
+    return texts
 # Next steps to be completed
 import urllib.request
 from urllib.error import HTTPError
@@ -13,8 +20,10 @@ from bs4 import BeautifulSoup
 source = urllib.request.urlopen(
         "https://stats.nba.com/teams/traditional/?sort=W_PCT&dir=-1").read()
 soup = BeautifulSoup(source, 'html.parser')
+date = getText(soup.find('body').select('td[class="first"]'))
 
-print(soup)
+
+print(date)
 
     
 #File_object = open("GameData.txt","w")
