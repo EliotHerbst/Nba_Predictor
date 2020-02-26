463a0f97326ec4c6be658201ebf61c1ede809230
EliotHerbst <39841180+EliotHerbst@users.noreply.github.com>
2020-02-26 13:54:40 -0500

    changed relu to leaky relu (64.342% (1.07%)

--- WebScrapers/BinaryClassification.py
+++ WebScrapers/BinaryClassification.py
@@ -1,7 +1,8 @@
+import keras
 from keras.callbacks import EarlyStopping
 from pandas import read_csv
 from keras.models import Sequential
-from keras.layers import Dense
+from keras.layers import Dense, LeakyReLU
 from keras.wrappers.scikit_learn import KerasClassifier
 from sklearn.model_selection import cross_val_score
 from sklearn.preprocessing import LabelEncoder
@@ -33,9 +34,12 @@ early_stopping_monitor = EarlyStopping(patience=20)
 def create_baseline():
     # create model
     model = Sequential()
-    model.add(Dense(28, input_dim=28, activation='relu'))
-    model.add(Dense(128, activation='relu'))
-    model.add(Dense(14, activation='relu'))
+    model.add(Dense(28, input_dim=28))
+    model.add(LeakyReLU(alpha=0.3))
+    model.add(Dense(128))
+    model.add(LeakyReLU(alpha=0.3))
+    model.add(Dense(14))
+    model.add(LeakyReLU(alpha=0.3))
     model.add(Dense(1, activation='sigmoid'))
     # Compile model
     model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
@@ -44,7 +48,7 @@ def create_baseline():
 
 # evaluate baseline model with standardized dataset
 # evaluate model with standardized dataset
-estimator = KerasClassifier(build_fn=create_baseline, epochs=100, batch_size=5, verbose=0)
+estimator = KerasClassifier(build_fn=create_baseline, epochs=100, batch_size=5, verbose=2)
 kfold = StratifiedKFold(n_splits=10, shuffle=True)
 results = cross_val_score(estimator, X, encoded_Y, cv=kfold)
 print("Baseline: %.2f%% (%.2f%%)" % (results.mean()*100, results.std()*100))
